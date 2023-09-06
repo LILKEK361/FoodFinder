61c2b9d4181fb7e18faa5a2c0343bcc885667a57
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-09-06 07:15:10 +0200

    Initial commit

---
+++ src/lib/codeAssets/Food.ts
@@ -0,0 +1,15 @@
+
+const FoodRequestApi = "https://api.edamam.com/api/food-database/v2/parser?app_id=825a1637&app_key=5eda8565e8c9ac9127281c6d01a79e64&ingr=Cheese&nutrition-type="
+
+
+
+export const getFoodRequestApi = (food : Array<string>) => {
+    return `https://api.edamam.com/api/food-database/v2/parser?app_id=825a1637&app_key=5eda8565e8c9ac9127281c6d01a79e64&ingr=${food}&nutrition-type=cooking`
+}
+
+export async  function getFood(api : string){
+    const request = await  fetch(api, )
+    const result = await request.json()
+    console.log(result.hints)
+    return request
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import {getFoodRequestApi, getFood} from "$lib/codeAssets/Food.ts";
+	import {onMount} from "svelte";
+    import {writable} from "svelte/store";
+    let input : any;
+    let foodOptions : any
+    const FoodSearch : any = writable([])
+
+
+    function searchFood () {
+       foodOptions = getFood(getFoodRequestApi($FoodSearch))
+        console.log(foodOptions)
+    }
+    function addToList(food : string){
+        if(input !==  ""){
+            $FoodSearch.push(food)
+            $FoodSearch = $FoodSearch
+            console.log(FoodSearch)
+            input = ""
+        }
+    }
+
+</script>
+
+
+<div class="h-[100vh] w-[100vw]">
+
+    <div class="h-[15vh] w-full bg-blue-950">
+        <input bind:value={input} type="text" placeholder="Enter your food ingredients: "  class="text-black">
+        <button on:click={addToList(input)} placeholder="Enter">{"Add Food"}</button>
+        <button on:click={searchFood} placeholder="Enter">{"Search Food"}</button>
+        <button on:click={searchFood} placeholder="Enter">{"Search Food"}</button>
+        <p>Ingredientslist:  {$FoodSearch}</p>
+    </div>
+
+</div>
