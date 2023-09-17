617496df14c38e3089c321678ce295851728557f
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-09-17 11:09:33 +0200

    Update CustomButtonElement.svelte

---
+++ src/lib/CustomButtonElement.svelte
@@ -0,0 +1,7 @@
+<details class="dropdown ">
+    <summary class="m-1 btn">Extra Options</summary>
+    <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+        <li>diet</li>
+        <li>3</li>
+    </ul>
+</details>
