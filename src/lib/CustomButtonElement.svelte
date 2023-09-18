205607245035d5401c59299a749d838ea0520399
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-09-18 12:43:39 +0200

    Setting up the button

--- src/lib/CustomButtonElement.svelte
+++ src/lib/CustomButtonElement.svelte
@@ -1,5 +1,13 @@
+<script lang="ts">
+    export let buttonContent : String
+    export let useFunction : any
+    export let design : string
+
+</script>
+
+
 <details class="dropdown ">
-    <summary class="m-1 btn">Extra Options</summary>
+    <summary class="m-1 btn">{buttonContent}</summary>
     <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
         <li>diet</li>
         <li>3</li>
